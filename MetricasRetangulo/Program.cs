3b5560b4ca96416ca0ef3e896a6e2837c0eb0b5e
José de Senna <josespnt1990@gmail.com>
2026-02-10 15:55:29 -0300

    feat: cria logica de uso da classe Retangulo
    - Solicita as medidas de largura e altura como entradas de console
    - Utiliza o metodo de exibicao da classe para saida de dados

--- MetricasRetangulo/Program.cs
+++ MetricasRetangulo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace MetricasRetangulo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Retangulo retangulo = new Retangulo();
+        
+        Console.WriteLine("Entre com a largura e a altura do retângulo:");
+        retangulo.Largura = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        retangulo.Altura = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.WriteLine(retangulo.ToString());
+    }
+}
